459111bebbfd360438fcb6d264a401affc353431
aleksmaz <62301593+aleksmaz@users.noreply.github.com>
2020-09-07 19:48:26 +0300

    Add files via upload

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://images.unsplash.com/photo-1509225770129-fbcf8a696c0b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80' />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            Posts
+            <div>
+                New post
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile
